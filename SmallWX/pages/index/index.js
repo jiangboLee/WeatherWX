9f7687f903cb8f4f44905c25ddd06ba61a4439f4
ljb48229 <ljb48229@ly.com>
2018-02-01 16:22:09 +0800

    网络请求封装，下拉刷新

--- SmallWX/pages/index/index.js
+++ SmallWX/pages/index/index.js
@@ -1,5 +1,7 @@
-//index.js
 //获取应用实例
+
+var network_util = require('../../utils/network.js');
+
 const app = getApp()
 var location;
 var isOpenSetting = false;
@@ -7,54 +9,64 @@ Page({
   data: {
     motto: 'nihao',
     location: '上海市',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    hasRefresh: false,
+    hehe: "aaa",
   },
-  //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
   gotest: function() {
     wx.navigateTo({
       url: '../scrollView/scrollView',
     })
   },
-
   //天气接口
   Weather: function(lat, longi) {
     var _this = this
-    wx.request({
-      // url: "https://api.seniverse.com/v3/weather/now.json",
-      // data: {
-      //   key: "ff8v8ynamw3oq3ax",
-      //   location: location ? "" :"shanghai",
-      //   language: "zh-Hans",
-      //   unit: "c"
-      // },
-      //和风
-      url: "https://free-api.heweather.com/s6/weather/now",
-      data: {
-        key: "bff5cc9bcfdf46b0a0e9bf0c260ff14f",
-        location: location ? longi + "," + lat : "shanghai",
-        lang: "en",
-        unit: "m"
-      },
-      header: {
-        'content-type': 'application/json'
-      },
-      method: "GET",
-      success: function (res) {
-        console.log(res.data)
-      },
-      fail: function (err) {
-        console.log(err)
-      },
-      complete: function () {
-        wx.hideLoading()
-      }
+    // wx.request({
+    //   // url: "https://api.seniverse.com/v3/weather/now.json",
+    //   // data: {
+    //   //   key: "ff8v8ynamw3oq3ax",
+    //   //   location: location ? "" :"shanghai",
+    //   //   language: "zh-Hans",
+    //   //   unit: "c"
+    //   // },
+    //   //和风
+    //   url: "https://free-api.heweather.com/s6/weather/now",
+    //   data: {
+    //     key: "bff5cc9bcfdf46b0a0e9bf0c260ff14f",
+    //     location: location ? longi + "," + lat : "shanghai",
+    //     lang: "en",
+    //     unit: "m"
+    //   },
+    //   header: {
+    //     'content-type': 'application/json'
+    //   },
+    //   method: "GET",
+    //   success: function (res) {
+    //     console.log(res.data)
+    //   },
+    //   fail: function (err) {
+    //     console.log(err)
+    //   },
+    //   complete: function () {
+    //     // 数据成功后，停止下拉刷新
+    //     wx.stopPullDownRefresh();
+    //     wx.hideLoading()
+    //   }
+    // })
+    var url = "https://free-api.heweather.com/s6/weather/now";
+    var data = {
+      key: "bff5cc9bcfdf46b0a0e9bf0c260ff14f",
+      location: location ? longi + "," + lat : "shanghai",
+      lang: "en",
+      unit: "m"
+    };
+    network_util._get(url, data, function(res){
+      console.log(res.data)
+    },function(res){
+
+    },function(){
+      // 数据成功后，停止下拉刷新
+      wx.stopPullDownRefresh();
+      wx.hideLoading()
     })
   },
   //地理反编码
@@ -154,41 +166,15 @@ Page({
       title: '天气',
       path: '/page/'
     }
+  },
+  refresh: function() {
+    this.setData({
+      hasRefresh: true,
+      hehe: bbbb
+    })
+    
+  },
+  onPullDownRefresh: function() {
+    this.Weather();
   }
-  
-  //   if (app.globalData.userInfo) {
-  //     this.setData({
-  //       userInfo: app.globalData.userInfo,
-  //       hasUserInfo: true
-  //     })
-  //   } else if (this.data.canIUse){
-  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-  //     // 所以此处加入 callback 以防止这种情况
-  //     app.userInfoReadyCallback = res => {
-  //       this.setData({
-  //         userInfo: res.userInfo,
-  //         hasUserInfo: true
-  //       })
-  //     }
-  //   } else {
-  //     // 在没有 open-type=getUserInfo 版本的兼容处理
-  //     wx.getUserInfo({
-  //       success: res => {
-  //         app.globalData.userInfo = res.userInfo
-  //         this.setData({
-  //           userInfo: res.userInfo,
-  //           hasUserInfo: true
-  //         })
-  //       }
-  //     })
-  //   }
-  // },
-  // getUserInfo: function(e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // }
 })
--- SmallWX/pages/scrollView/scrollView.js
+++ SmallWX/pages/scrollView/scrollView.js
@@ -1,31 +1,15 @@
 var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
-    toView: 'red',
-    scrollTop: 100
+    hasRefresh: false,
   },
-  upper: function (e) {
-    console.log(e)
+  onLoad: function() {
+
   },
-  lower: function (e) {
-    console.log(e)
-  },
-  scroll: function (e) {
-    console.log(e)
-  },
-  tap: function (e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
-        this.setData({
-          toView: order[i + 1]
-        })
-        break
-      }
-    }
-  },
-  tapMove: function (e) {
+  refesh: function() {
     this.setData({
-      scrollTop: this.data.scrollTop + 10
+      hasRefresh: true
     })
+    var url=""
   }
 })
